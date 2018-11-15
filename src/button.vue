eec2f1d7642e8495a585b27212e75ae4cb604212
liuzhao <1257625996@qq.com>
2018-11-15 09:50:45 +0800

    add button icon

--- src/button.vue
+++ src/button.vue
@@ -1,12 +1,31 @@
 <template>
-  <button class="g-button">Hi</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <g-icon class="icon" :name="icon" v-if="icon"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
   export default {
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator(val) {
+          return val === 'left' || val === 'right'
+        }
+      }
+    }
   }
 </script>
 <style scoped lang="scss">
   .g-button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     font-size: var(--font-size);
     height: var(--button-height);
     padding: 0 1em;
@@ -22,5 +41,13 @@
     &:focus {
       outline: none;
     }
+    > .icon { order: 1; margin-right: .3em; }
+    > .content { order: 2; }
+
+    &.icon-right {
+      > .icon { order: 2;  margin-right: 0; margin-left: .3em; }
+      > .content { order: 1; }
+    }
   }
+
 </style>
