031dc026ae56c54a2bc78e3670f14a8b7459a16a
liuzhao <1257625996@qq.com>
2018-11-15 10:17:17 +0800

    add test modules

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
+import chai from 'chai';
 import Button from './button.vue';
 import Icon from './icon.vue';
+
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
@@ -8,3 +10,17 @@ Vue.component('g-icon', Icon);
 const app = new Vue({
   el: '#app',
 });
+{
+  const expert = chai.expect;
+  // 单元测试
+  const Constructor = Vue.extend(Button);
+  const button = new Constructor({
+    propsData: {
+      icon: 'setting',
+    },
+  });
+  button.$mount('#test');
+  const useEelement = button.$el.querySelector('use');
+  const href = useEelement.getAttribute('xlink:href');
+  expert(href).to.eq('#i-setting');
+}
