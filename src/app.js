013e9b14d6208f9cbc875ca58ae776137a8dec62
liuzhao <liuzhao@lnkun.com>
2019-02-19 17:54:16 +0800

    完成ButtonGroup组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import Button from './Button';
+import ButtonGroup from './Button-group';
 import Icon from './Icon';
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
+Vue.component('g-button-group', ButtonGroup);
+
 
 new Vue({
   el: '#app',
