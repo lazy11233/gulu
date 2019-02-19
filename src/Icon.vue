6b2f6dd13da91ff9347a7264c8d7fc5840cff085
liuzhao <liuzhao@lnkun.com>
2019-02-19 15:42:15 +0800

    将svg代码整理到Icon组件，更新Button组件中的Icon

---
+++ src/Icon.vue
@@ -0,0 +1,23 @@
+<template>
+<svg class="g-icon">
+  <use :xlink:href="`#i-${name}`"></use>
+</svg>
+</template>
+<script>
+export default {
+  name: 'g-icon',
+  props: {
+    name: {
+      required: true,
+      type: String
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.g-icon {
+  height: 1em;
+  width: 1em;
+}
+</style>
+
