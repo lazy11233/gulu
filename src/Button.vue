e6be6510b464fdfcfc928e39679798afb8e188c3
liuzhao <liuzhao@lnkun.com>
2019-02-19 14:03:31 +0800

    加入 parcel，将button文件改为单文件组件

---
+++ src/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+<script>
+export default {
+  name: 'g-button',
+}
+</script>
+<style lang="scss" scoped>
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border: 1px solid var(--border-color);
+  border-radius: var(--border-radius);
+  background-color: var(--button-bg);
+
+  &:hover { border-color: var(--border-color-hover); }
+
+  &:active { background: var(--button-bg-active); }
+
+  &:focus { outline: none; }
+}
+</style>
