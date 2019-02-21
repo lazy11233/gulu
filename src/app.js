436eec97de6ddeff384088f9da9626ca32ebeac3
liuzhao <liuzhao@lnkun.com>
2019-02-21 16:22:01 +0800

    添加 g-input 组件

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import Button from './Button';
 import ButtonGroup from './Button-group';
 import Icon from './Icon';
+import Input from './input';
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
 Vue.component('g-button-group', ButtonGroup);
+Vue.component('g-input', Input);
 
 
 new Vue({
---
+++ src/input.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <input type="text" placeholder="请输入内容">
+  </div>
+</template>
+<script>
+  export default {
+    name: 'input'
+  }
+</script>
+<style lang="scss" scoped>
+
+</style>
