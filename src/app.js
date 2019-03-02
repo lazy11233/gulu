6e98b6c981697061240781175525bb49b948f86f
liuzhao <liuzhao@lnkun.com>
2019-03-02 15:39:09 +0800

    row 和 col 的雏形完成

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ import Button from './Button';
 import ButtonGroup from './Button-group';
 import Icon from './Icon';
 import Input from './input';
+import Row from './row';
+import Col from './col';
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
 Vue.component('g-button-group', ButtonGroup);
 Vue.component('g-input', Input);
-
+Vue.component('g-row', Row);
+Vue.component('g-col', Col);
 
 new Vue({
   el: '#app',
---
+++ src/col.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot>
+
+    </slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'g-col',
+    props: {
+      span: {
+        type: Number,
+        required: false
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .col {
+    height: 100px;
+    background-color: grey;
+    border: 1px solid red;
+    width: 50%;
+  }
+
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    .#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+</style>
